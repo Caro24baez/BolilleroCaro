7de0c02fb904289abac98fbb30ddde6711944ce8
Caro24baez <caro24baez@gmailcom>
2020-11-11 15:46:10 -0300

    1er commit

---
+++ BolilleroCaro/Bolillero.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace BolilleroCaro
+{
+    public class Bolillero
+    {
+        public List<int> bolillasAdentro { get; set; }
+
+        public List<int> bolillasAfuera{ get; set; }
+
+        public int cantBolillas { get; set; }
+
+        Random numRand = new Random();
+
+        public Bolillero(int inico, int fin)
+        {
+            bolillasAdentro = new List<int>();
+            bolillasAfuera = new List<int>();
+        }
+
+        public Bolillero()
+        {
+        }
+
+        public void llenarBolillero(int inicio, int fin)
+        {
+            for (int i = 0; i < cantBolillas; i++)
+            {
+                bolillasAdentro.Add(i);
+            }
+        }
+
+        public void regresarBolillas()
+        {
+            bolillasAdentro.AddRange(bolillasAfuera);
+            bolillasAfuera.Clear();
+        }
+
+
+        public int sacarBolilla()
+        {
+            int bolilla = bolillasAdentro[numRand.Next(bolillasAdentro.Count)];
+            bolillasAfuera.Add(bolilla);
+            bolillasAdentro.Remove(bolilla);
+            return bolilla;
+        }
+
+       
+        }
+    }
+
---
+++ BolilleroCaro/Simulacion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BolilleroCaro
+{
+    public class Simulacion
+    {
+        Bolillero bolillero = new Bolillero();
+        public bool Jugar(List<int> jugadas)
+        {
+            var comparar = 0;
+            bolillero.regresarBolillas();
+            foreach (var bolillaJugada in jugadas)
+            {
+                comparar = bolillero.sacarBolilla();
+                if (comparar != bolillaJugada)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public int JugarNVeces(List<int> jugadas, int cantVeces)
+        {
+            int cantGanados = 0;
+            for (int i = 0; i < cantVeces; i++)
+            {
+
+                if (this.Jugar(jugadas) == true)
+                {
+                    cantGanados++;
+                }
+
+
+            }
+            return cantGanados;
+
+        }
+    }
+}
