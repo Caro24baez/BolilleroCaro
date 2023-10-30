71e2dc5a965f1777e33442a2374866980e1d7459
Caro <48722102+Caro24baez@users.noreply.github.com>
2023-10-29 21:13:59 -0300

    cambios en simulacion

--- BolilleroCaro/Simulacion.cs
+++ BolilleroCaro/Simulacion.cs
@@ -1,35 +1,32 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 namespace BolilleroCaro
 {
     public class Simulacion
     {
-        Bolillero bolillero = new Bolillero();
-        public bool Jugar(List<int> jugadas)
+        
+        private Bolillero bolillero;
+      //  private IRandomNumberGenerator azar;
+
+        private readonly IRandomNumberGenerator azar;
+        public Simulacion(Bolillero bolillero, IRandomNumberGenerator azar) // toma un bolillero y un generador de números aleatorios
         {
-            var comparar = 0;
-            bolillero.regresarBolillas();
-            foreach (var bolillaJugada in jugadas)
-            {
-                comparar = bolillero.sacarBolilla();
-                if (comparar != bolillaJugada)
-                {
-                    return false;
-                }
-            }
-            return true;
+            this.bolillero = bolillero;
+            this.azar = azar;
         }
+        public bool Jugar(List<int> jugadas) => bolillero.sacarJugada(jugadas);
+
 
         public int JugarNVeces(List<int> jugadas, int cantVeces, Bolillero bolillero)
         {
             int cantGanados = 0;
             for (int i = 0; i < cantVeces; i++)
             {
-
+                bolillero.regresarBolillas();
                 if (this.Jugar(jugadas) == true)
                 {
                     cantGanados++;
@@ -40,24 +37,94 @@ namespace BolilleroCaro
             return cantGanados;
 
         }
-        public int simularSinHilos(Bolillero bolillero, List<int> jugadas, int CantSimulación, int cantVeces)
+        public long SimularSinHilos(Bolillero bolillero, List<int> jugada, int cantSimulaciones)
         {
-            return JugarNVeces(jugadas, cantVeces, bolillero);
-        }
-        public int simularConHilos(Bolillero bolillero, List<int> jugadas, int CanSimulación, int canHilos, int cantVeces)
-        {
-            var vectorTarea = new Task<int>[canHilos];
-            int resto = cantVeces % canHilos;
-            for (int i = 0; i < canHilos; i++)
+            long cantGanados = 0;
+            for (int i = 0; i < cantSimulaciones; i++)
             {
-                Bolillero clon = (Bolillero)bolillero.Clone();
-                vectorTarea[i] = Task.Run(() => JugarNVeces(jugadas, cantVeces / canHilos, clon));
+                Bolillero bolilleroClone = (Bolillero)bolillero.Clone();
+                bolilleroClone.regresarBolillas();
+                if (bolilleroClone.sacarJugada(jugada))
+                {
+                    cantGanados++;
+                }
             }
-            Task.WaitAll(vectorTarea);
-            Bolillero clon1 = (Bolillero)bolillero.Clone();
-            vectorTarea[canHilos - 1] = Task.Run(() => JugarNVeces(jugadas, cantVeces / canHilos + resto , clon1));
-            return vectorTarea.Sum(T => T.Result);
+            return cantGanados;
             
         }
+        public int SimularConHilos(Bolillero bolillero, List<int> jugada, int jugarNVeces, int cantHilos)
+        {
+            var vectorTarea = new Task<int>[cantHilos];
+            for (int i = 0; i < cantHilos; i++)
+            {
+                Bolillero clon = (Bolillero)bolillero.Clone();
+                vectorTarea[i] = Task.Run(() =>
+                {
+                    int ganadosEnHilo = 0;
+                    for (int j = 0; j < jugarNVeces / cantHilos; j++)
+                    {
+                        lock (clon) // Acceso seguro al bolillero
+                        {
+                            clon.regresarBolillas();
+                            if (clon.sacarJugada(jugada))
+                            {
+                                ganadosEnHilo++;
+                            }
+                        }
+                    }
+                    return ganadosEnHilo;
+                });
+            }
+            Task.WaitAll(vectorTarea);
+            
+            foreach (var tarea in vectorTarea)
+            {
+                if (tarea.Exception != null)
+                {
+                    Console.WriteLine("Ocurrió un error durante la simulación: " + tarea.Exception.Message);
+                    return 0; 
+                }
+            }
+
+            int totalGanados = vectorTarea.Sum(t => t.Result);
+            return totalGanados;
+        }
+        public async Task<long> SimularConHilosAsync(Bolillero bolillero, List<int> jugada, int cantSimulaciones, int cantHilos)
+        {
+            var tasks = new List<Task<long>>();
+
+            for (int i = 0; i < cantHilos; i++)
+            {
+                Bolillero bolilleroClone = (Bolillero)bolillero.Clone();
+                tasks.Add(SimularHiloAsync(bolilleroClone, jugada, cantSimulaciones / cantHilos));
+            }
+
+            long totalGanados = 0;
+            foreach (var task in tasks)
+            {
+                totalGanados += await task.ConfigureAwait(false);
+            }
+
+            return totalGanados;
+        }
+
+        private async Task<long> SimularHiloAsync(Bolillero bolillero, List<int> jugada, int cantSimulaciones)
+        {
+            long cantGanados = 0;
+            for (int i = 0; i < cantSimulaciones; i++)
+            {
+                bolillero.regresarBolillas(); // Restablece el estado del bolillero clonado si es necesario
+                if (bolillero.sacarJugada(jugada))
+                {
+                    cantGanados++;
+                }
+            }
+            return cantGanados;
+        }
+
+       /* public int SimularConHilos(Bolillero bolillero, List<int> jugada, int jugarNVeces, int cantHilos)
+        {
+            throw new NotImplementedException();
+        }*/
     }
 }
